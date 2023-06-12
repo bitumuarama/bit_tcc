db4ee0e810bab40223e887393736ef512da80757
brBertolli <88241260+brBertolli@users.noreply.github.com>
2023-06-12 17:42:37 -0300

    120620231742

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<?php
+// Verificar se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Obter as informações do formulário
+    //  $username = $_POST['username'];
+    //  $password = $_POST['password'];
+
+    // Conectar ao banco de dados
+    // $servername = "localhost";
+    // $database = "bit_tcc";
+    // $username_db = "root";
+    // $password_db = "";
+
+    // $conn = new mysqli($servername, $username_db, $password_db, $database);
+
+    // Verificar se a conexão foi estabelecida com sucesso
+    // if ($conn->connect_error) {
+    //     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    //  }
+
+    // Escapar caracteres especiais para evitar injeção de SQL
+    //  $username = $conn->real_escape_string($username);
+    //  $password = $conn->real_escape_string($password);
+
+    // Consulta SQL para verificar as credenciais (case-sensitive)
+    //  $sql = "SELECT * FROM usuario WHERE nome = '$username' AND senha = '$password'";
+    // $result = $conn->query($sql);
+
+    // Verificar se a consulta retornou algum resultado
+    //if ($result->num_rows === 1) {
+    // Credenciais válidas, redirecionar para a página de controle
+    //echo $password;
+    // echo $username;
+    header("Location: pages/sistema.php");
+    //    exit();
+    // } else {
+    // Credenciais inválidas, exibir mensagem de erro
+    //    $error_message = "Usuário ou Senha inválidos!";
+    // }
+
+    // Fechar a conexão com o banco de dados
+    // $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -16,7 +62,7 @@
         <div class="logo-container">
             <img src="assets/img/logo_bit_300x150px.svg" alt="Logo da Empresa">
         </div>
-        <form method="POST" action="assets/php/login.php">
+        <form method="POST">
             <label for="username">Usuário:
 
             </label>
@@ -29,9 +75,10 @@
                 <p class="login-erro">
                     <?php echo $error_message; ?>
                 </p>
-                    <?php endif; ?>
+            <?php endif; ?>
 
             <button type="submit">Entrar</button>
+            <p>Configurar Script!!!</p>
         </form>
     </div>
 </body>
--- pages/sistema.php
+++ pages/sistema.php
@@ -1,19 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/sistema.css">
 </head>
+
 <body>
-    <h2>SISTEMA</h2>
+    <div class="container">
 
-    <h3>Cadastrar</h3>
-    <a href="">Usuário</a>
-    <a href="">Cliente</a>
-    <a href="">Produto</a>
+        <header>
+            <h1>Bem-Vindo ao Sistema!</h1>
+            <p>Informações básicas da página e do Usuário...</p>
+        </header>
 
-    
+        <div class="cont1">
+            <div class="cont2">
+                <h1>PAINEL DE CONTROLE</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
+            </div>
+
+            <ul>
+                <h2>Cadastros</h2>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+
+            <ul>
+                <h2>Cadastros</h2>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+
+            <ul>
+                <h2>Cadastros</h2>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+                <li>Cadastrar ABC</li>
+
+            </ul>
+        </div>
+        <div class="int">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor"
+                class="bi bi-question-circle-fill" viewBox="0 0 50 50">
+                <path
+                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
+            </svg>
+        </div>
 </body>
+
 </html>
